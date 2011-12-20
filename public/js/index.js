1158d36a8cdec0ff57338205a113259008f88574
Valentyn Maksymov <valik.maksimov@gmail.com>
2011-12-20 08:31:56 +0200

    Added 3 views

--- public/js/index.js
+++ public/js/index.js
@@ -1,78 +1,107 @@
 /**
  * @author Valik
  */
-var Start = Backbone.View.extend({
-    ggg: {
-        "click": "check" // Обработчик клика на кнопке "Проверить"
-    },
+
+
+
+(function () {
+	Controller = Backbone.Router.extend({
+		
+	    routes: {
+	        "": "start", // Пустой hash-тэг
+	        "success": "success", // Блок удачи
+	        "error": "error" // Блок ошибки
+	    },	    
+	
+	    start: function () {
+	        if (Views.start != null) {
+	            Views.start.render();
+	        }
+	    },
+	
+	    success: function () {
+	        if (Views.success != null) {
+	            Views.success.render();
+	        }
+	    },
+	
+	    error: function () {
+	        if (Views.error != null) {
+	            Views.error.render();
+	        }
+	    }
+	});
+})();
+
+(function () {
+	Start = Backbone.View.extend ({
+		el: $('#home'),
+		
+		template: _.template($('#start').html()),
+		
+		events : {
+			"click input:button": "_onButtonClick"
+		},
+		
+		_onButtonClick : function () {
+	    	AppState.username = this.el.find("input:text").val(); // Сохранение имени пользователя
+	        if (_.detect(Family, function (elem) { return elem == AppState.username; })) // Проверка имени пользователя
+	            controller.navigate("success", true); // переход на страницу success
+	        else
+	            controller.navigate("error", true); // переход на страницу error	
+	   },
+	   
+	   render: function () {
+			$(this.el).html(this.template({}));
+	   }
+	   
+	});
+})();
+
+(function () {
+	Success = Backbone.View.extend ({
+	
+		el: $('#home'),
+			
+		template: _.template($('#success').html()),
+		
+		render: function () {
+		  	$(this.el).html(this.template({}));
+		}
+	});
+})();
+
+(function () {
+	Error = Backbone.View.extend({
+		
+		el: $('#home'),
+		
+		template: _.template($('#error').html()),
+		
+		render: function () {
+		  	$(this.el.html(this.template({})))
+		}
+	});
+})();
+
+$(document).ready(function () {
+	
+	Views = { 
+        start: new Start(),
+        success: new Success(),
+        error: new Error()
+    };
     
-    initialize: function () {
-    	debugger
-    	this.delegateEvents (this.ggg);
-    },
-    
-    start : function () {},
-    
-    check: function () {
-    	alert('test')
-    	debugger
-        if (this.el.find("input:text").val() == "test") // Проверка текста
-            controller.navigate("success", true); // переход на страницу success
-        else
-            controller.navigate("error", true); // переход на страницу error
-    }
+    Family = ["Валик", "Андрей", "Ника"];
+	
+	AppState = {
+	    username: ""
+	};
+
+	controller = new Controller(); // Создаём контроллер
+	Backbone.history.start();  // Запускаем HTML5 History push 
 });
 
-var Controller = Backbone.Router.extend({
-    routes: {
-        "": "start", // Пустой hash-тэг
-        "success": "success", // Блок удачи
-        "error": "error" // Блок ошибки
-    },
-
-    start: function () {
-    	debugger
-    	new Start({el : $("#home")});
-        $(".block").hide(); // Прячем все блоки
-        $("#start").show(); // Показываем нужный
-    },
-
-    success: function () {
-        $(".block").hide();
-        $("#success").show();
-    },
-
-    error: function () {
-        $(".block").hide();
-        $("#error").show();
-    }
-});
-
-var controller = new Controller(); // Создаём контроллер
-
-Backbone.history.start();  // Запускаем HTML5 History push 
-
-
-
-var Start = Backbone.View.extend({
-    el: $("#start"), // DOM элемент widget'а
-    events: {
-        "click .button": "check" // Обработчик клика на кнопке "Проверить"
-    },
-    
-    initialize: function () {
-    	this.delegateEvents (events);
-    },
-    
-    check: function () {
-    	alert('test')
-    	debugger
-        if (this.el.find("input:text").val() == "test") // Проверка текста
-            controller.navigate("success", true); // переход на страницу success
-        else
-            controller.navigate("error", true); // переход на страницу error
-    }
-});
 
 
 
